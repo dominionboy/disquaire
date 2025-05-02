b58b91db7b02d4a809e216f0af45513a2b7b896e
coffeebreak2105 <lara.vorlet@gmail.com>
2025-05-02 15:44:23 +0200

    Correction des alter

--- disquaire.sql
+++ disquaire.sql
@@ -265,109 +265,70 @@ CREATE TABLE nationalite (
 );
 
 -- Ajouter les contraintes de clés étrangères sur les différentes tables
-ALTER TABLE 
-  ADD CONSTRAINT
-  FOREIGN KEY ()
-  REFERENCES  ()
+ALTER TABLE abonnement
+   ADD CONSTRAINT fk_abonnement_type_abonnement
+   FOREIGN KEY (id_type_abonnement)
+   REFERENCES type_abonnement(id_type_abonnement);
 
-ALTER TABLE categorie
-  ADD CONSTRAINT fk_categorie_produit
-  FOREIGN KEY (id_produit)
-  REFERENCES produit (id_produit)
+ALTER TABLE abonnement_actif
+   ADD CONSTRAINT fk_abonnement_actif_abonnement
+   FOREIGN KEY (id_abonnement)
+   REFERENCES abonnement(id_abonnement);
 
-ALTER TABLE categorie
-  ADD CONSTRAINT fk_categorie_genre
-  FOREIGN KEY (id_genre)
-  REFERENCES genre (id_genre)
+ALTER TABLE abonnement_actif
+   ADD CONSTRAINT fk_abonnement_actif_client
+   FOREIGN KEY (numero_client, id_personne)
+   REFERENCES client(numero_client, id_personne);
 
-ALTER TABLE disponible
-  ADD CONSTRAINT fk_disponible_format
-  FOREIGN KEY (id_format)
-  REFERENCES format (id_format)
+ALTER TABLE paiement
+   ADD CONSTRAINT fk_paiement_client
+   FOREIGN KEY (numero_client, id_personne)
+   REFERENCES client(numero_client, id_personne);
 
-ALTER TABLE disponible
-  ADD CONSTRAINT fk_disponible_produit
-  FOREIGN KEY (id_produit)
-  REFERENCES produit (id_produit)
-
-ALTER TABLE produit
-  ADD CONSTRAINT fk_produit_type
-  FOREIGN KEY (id_type)
-  REFERENCES type (id_type)
-
-ALTER TABLE coordonnee
-  ADD CONSTRAINT
-  FOREIGN KEY (id_pays)
-  REFERENCES pays (id_pays)
-
-ALTER TABLE coordonnee
-  ADD CONSTRAINT fk_coordonnee_localite
-  FOREIGN KEY (npa)
-  REFERENCES locallite (npa)
+ALTER TABLE paiement
+   ADD CONSTRAINT fk_paiement_methode_paiement
+   FOREIGN KEY (id_methode_paiement)
+   REFERENCES methode_paiement(id_methode_paiement);
 
 ALTER TABLE client
   ADD CONSTRAINT fk_client_coordonnee
   FOREIGN KEY (id_coordonnee)
-  REFERENCES coordonnee (id_coordonnee)
+  REFERENCES coordonnee(id_coordonnee)
 
-ALTER TABLE fournisseur
-  ADD CONSTRAINT fk_fournisseur_coordonnee
-  FOREIGN KEY (id_coordonnee)
-  REFERENCES coordonnee (id_coordonnee)
+ALTER TABLE reservation
+   ADD CONSTRAINT fk_reservation_exemplaire
+   FOREIGN KEY (id_produit, code_interne)
+   REFERENCES exemplaire(id_produit, code_interne);
 
-ALTER TABLE commande
-  ADD CONSTRAINT fk_commande_fournisseur
-  FOREIGN KEY (id_fournisseur)
-  REFERENCES fournisseur (id_fournisseur)
+ALTER TABLE reservation
+   ADD CONSTRAINT fk_reservation_client
+   FOREIGN KEY (numero_client, id_personne)
+   REFERENCES client(numero_client, id_personne);
 
+ALTER TABLE emprunt
+   ADD CONSTRAINT fk_emprunt_client
+   FOREIGN KEY (numero_client, id_personne)
+   REFERENCES client(numero_client, id_personne);
 
-ALTER TABLE quantite
-  ADD CONSTRAINT fk_quantite_commande
-  FOREIGN KEY (id_commande,id_fournisseur)
-  REFERENCES commande (id_commande,id_fournisseur)
+ALTER TABLE emprunt
+   ADD CONSTRAINT fk_emprunt_exemplaire
+   FOREIGN KEY (id_produit, code_interne)
+   REFERENCES exemplaire(id_produit, code_interne);
 
-
-ALTER TABLE quantite
-  ADD CONSTRAINT fk_quantite_produit
-  FOREIGN KEY (id_produit)
-  REFERENCES produit (id_produit)
-
-
-ALTER TABLE nationalite
-  ADD CONSTRAINT fk_nationalite_pays
-  FOREIGN KEY (id_pays)
-  REFERENCES pays (id_pays)
-
-
-ALTER TABLE nationalite
-  ADD CONSTRAINT fk_nationalite_artiste
-  FOREIGN KEY (id_artiste,id_personne)
-  REFERENCES artiste (id_artitste,id_personne)
-
-
-ALTER TABLE activite
-  ADD CONSTRAINT fk_activite_type_artiste
-  FOREIGN KEY (id_type_artiste)
-  REFERENCES artiste (id_type_artiste);
-
-
-ALTER TABLE activite
-  ADD CONSTRAINT fk_activite_artiste
-  FOREIGN KEY (id_artiste,id_personne)
-  REFERENCES artiste (id_artiste, id_personne);
-
-ALTER TABLE creation
-   ADD CONSTRAINT fk_creation_produit
+ALTER TABLE exemplaire
+   ADD CONSTRAINT fk_exemplaire_produit
    FOREIGN KEY (id_produit)
    REFERENCES produit(id_produit);
 
+ALTER TABLE exemplaire
+   ADD CONSTRAINT fk_exemplaire_etat
+   FOREIGN KEY (id_etat)
+   REFERENCES etat(id_etat);
 
-ALTER TABLE creation
-   ADD CONSTRAINT fk_creation_artiste
-   FOREIGN KEY (id_artiste,id_personne)
-   REFERENCES artiste(id_artiste,id_personne);
-
-
+ALTER TABLE changement_stock
+   ADD CONSTRAINT fk_changement_stock_prdoduit
+   FOREIGN KEY (id_produit)
+   REFERENCES produit(id_produit);
 
 ALTER TABLE parler
    ADD CONSTRAINT fk_parler_langue
@@ -379,96 +340,88 @@ ALTER TABLE parler
    FOREIGN KEY (id_produit)
    REFERENCES produit(id_produit);
 
+ALTER TABLE produit
+  ADD CONSTRAINT fk_produit_type
+  FOREIGN KEY (id_type)
+  REFERENCES type(id_type)
 
+ALTER TABLE disponible
+  ADD CONSTRAINT fk_disponible_format
+  FOREIGN KEY (id_format)
+  REFERENCES format(id_format)
 
-ALTER TABLE changement_stock
-   ADD CONSTRAINT fk_changement_stock_prdoduit
-   FOREIGN KEY (id_produit)
-   REFERENCES client(id_produit);
+ALTER TABLE disponible
+  ADD CONSTRAINT fk_disponible_produit
+  FOREIGN KEY (id_produit)
+  REFERENCES produit(id_produit)
 
+ALTER TABLE categorie
+  ADD CONSTRAINT fk_categorie_produit
+  FOREIGN KEY (id_produit)
+  REFERENCES produit(id_produit)
 
+ALTER TABLE categorie
+  ADD CONSTRAINT fk_categorie_genre
+  FOREIGN KEY (id_genre)
+  REFERENCES genre(id_genre)
 
+ALTER TABLE quantite
+  ADD CONSTRAINT fk_quantite_commande
+  FOREIGN KEY (id_commande, id_fournisseur)
+  REFERENCES commande(id_commande, id_fournisseur)
 
+ALTER TABLE quantite
+  ADD CONSTRAINT fk_quantite_produit
+  FOREIGN KEY (id_produit)
+  REFERENCES produit(id_produit)
 
+ALTER TABLE commande
+  ADD CONSTRAINT fk_commande_fournisseur
+  FOREIGN KEY (id_fournisseur)
+  REFERENCES fournisseur(id_fournisseur)
 
+ALTER TABLE fournisseur
+  ADD CONSTRAINT fk_fournisseur_coordonnee
+  FOREIGN KEY (id_coordonnee)
+  REFERENCES coordonnee(id_coordonnee)
 
-ALTER TABLE emprunt
-   ADD CONSTRAINT fk_emprunt_client
-   FOREIGN KEY (numero_client, id_personne)
-   REFERENCES client(numero_client, id_personne);
+ALTER TABLE coordonnee
+  ADD CONSTRAINT fk_coordonnee_pays
+  FOREIGN KEY (id_pays)
+  REFERENCES pays(id_pays)
 
+ALTER TABLE coordonnee
+  ADD CONSTRAINT fk_coordonnee_localite
+  FOREIGN KEY (npa)
+  REFERENCES localite(npa)
 
-
-
-ALTER TABLE emprunt
-   ADD CONSTRAINT fk_emprunt_exemplaire
-   FOREIGN KEY (code_interne, id_produit)
-   REFERENCES exemplaire(code_interne, id_produit);
-
-
-
-ALTER TABLE exemplaire
-   ADD CONSTRAINT fk_exemplaire_produit
+ALTER TABLE creation
+   ADD CONSTRAINT fk_creation_produit
    FOREIGN KEY (id_produit)
    REFERENCES produit(id_produit);
 
+ALTER TABLE creation
+   ADD CONSTRAINT fk_creation_artiste
+   FOREIGN KEY (id_artiste, id_personne)
+   REFERENCES artiste(id_artiste, id_personne);
 
+ALTER TABLE activite
+  ADD CONSTRAINT fk_activite_type_artiste
+  FOREIGN KEY (id_type_artiste)
+  REFERENCES type_artiste(id_type_artiste);
 
+ALTER TABLE activite
+  ADD CONSTRAINT fk_activite_artiste
+  FOREIGN KEY (id_artiste, id_personne)
+  REFERENCES artiste(id_artiste, id_personne);
 
+ALTER TABLE nationalite
+  ADD CONSTRAINT fk_nationalite_pays
+  FOREIGN KEY (id_pays)
+  REFERENCES pays(id_pays)
 
-ALTER TABLE exemplaire
-   ADD CONSTRAINT fk_exemplaire_etat
-   FOREIGN KEY (id_etat)
-   REFERENCES etat(id_etat);
-
-
-
-
-
-
-
-ALTER TABLE reservation
-   ADD CONSTRAINT fk_contrat_exemplaire
-   FOREIGN KEY (code_interne, id_produit)
-   REFERENCES exemplaire(ide, id_produit);
-
-
-
-
-ALTER TABLE reservation
-   ADD CONSTRAINT fk_reservation_client
-   FOREIGN KEY (numero_client, id_personne)
-   REFERENCES client(numero_client, id_personne);
-
-
-ALTER TABLE paiement
-   ADD CONSTRAINT fk_paiement_client
-   FOREIGN KEY (numero_client,id_personne)
-   REFERENCES client(id_personne);
-
-ALTER TABLE paiement
-   ADD CONSTRAINT fk_paiement_methode_paiement
-   FOREIGN KEY (id_methode_paiement)
-   REFERENCES methode_paiementduit(id_methode_paiement);
-
-
-
-
-ALTER TABLE abonnement_actif
-   ADD CONSTRAINT fk_abonnement_actif_client
-   FOREIGN KEY (numero_client, id_personne)
-   REFERENCES client(id_anumero_clietrtiste, id_personne);
-
-
-
-
-ALTER TABLE abonnement_actif
-   ADD CONSTRAINT fk_abonnement_actif_abonnement
-   FOREIGN KEY (id_abonnement)
-   REFERENCES abonnement(id_abonnement);
-
-
-ALTER TABLE abonnement
-   ADD CONSTRAINT fk_abonnement_type_abonnement
-   FOREIGN KEY (id_type_abonnement)
-   REFERENCES type_abonnement(id_type_abonnement);
+ALTER TABLE nationalite
+  ADD CONSTRAINT fk_nationalite_artiste
+  FOREIGN KEY (id_artiste, id_personne)
+  REFERENCES artiste(id_artiste, id_personne)
+  
